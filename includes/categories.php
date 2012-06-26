5386a46c95270694020683c2add00d0199193638
Abdoulaye Camara <lilbro1062000@gmail.com>
2012-06-26 00:54:15 -0400

    something new

--- includes/categories.php
+++ includes/categories.php
@@ -1,22 +1,22 @@
-
+<ul id="Categories" class="grid_4">
 <?php
 
-// $results = ex_query("Select upper(name) from categories order by ID");
-// 
-// while ($row = mysql_fetch_array($results))
-// {
-	// echo "<ul id=\"Categories\" class=\"grid_3\">";
-    // echo ("\n<li><a href=\"browse.php?Category=" . urlencode($row[0]) . "\">" . $row[0] .
-        // "</a></li> \n");
-	// echo "</ul>";	
-// }
+
+$results = ex_query("Select upper(name) from categories order by ID");
+
+while ($row = mysql_fetch_array($results))
+{
+//	echo "<ul id=\"Categories\" class=\"grid_3\">";
+    echo ("\n<li><a href=\"browse.php?Category=" . urlencode($row[0]) . "\">" . $row[0] .
+        "</a></li> \n");
+//	echo "</ul>";	
+}
 ?>    
 <!-- <li>================</li> -->
 
-<ul id="Categories" class="grid_4">
+
 
 <li>
-	CATEGORIES
 	<a href="reco.php">RECOMMEND</a>
 </li>
 </ul>
--- index.php
+++ index.php
@@ -3,7 +3,7 @@ include_once ("includes/head.php");
 include_once ("includes/categories.php");
 ?>
 <div id="fb-root"></div>
-<!--
+
 <script>(function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0];
   if (d.getElementById(id)) return;
@@ -11,9 +11,12 @@ include_once ("includes/categories.php");
   js.src = "//connect.facebook.net/en_US/all.js#xfbml=1&appId=453077271378502";
   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));</script>
--->
-<div id="mainPage" class="grid_12 alpha">
-	<img src="images/Mark-Fletcher-Haleakala-960x900.jpg" />
+
+<div id="mainPage" class="grid_12">
+	<img src="images/Mark-Fletcher-Haleakala-960x900.jpg" width="940"/>
+	<video
+	<?php ?>
+	></video>
 <!--
 	<table>
 		<tr>
@@ -49,18 +52,40 @@ include_once ("includes/categories.php");
   ?>
 </div>
 <div id= "side" class="grid_6">
-	 a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	  a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	          a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	           a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	           
+	<div class="fb-activity" data-site="www.TMSomething.com" data-app-id="<?php echo FB_APP_ID; ?>" data-width="460" data-height="300" data-header="true" data-recommendations="false"></div>
 </div>
 <div id= "side" class="grid_6">
-	 a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	  a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	          a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	           a whole vunch of bhosndfijks df sihdf ;akshf as;ihdf;'askdf s df sdifh wenfwmen row;e hrwen f;ah efna89wef20[34n r;dmnf ;a'hwefpaqw df'jakns'; eifya0[we8 hfa;sndf ';aw90efh 'ankjef;aiw e][fhj aw'e hf]]
-	           
+	 <script charset="utf-8" src="http://widgets.twimg.com/j/2/widget.js"></script>
+<script>
+new TWTR.Widget({
+  version: 2,
+  type: 'search',
+  search: '#TMSomething OR @TMSomething',
+  interval: 10000,
+  title: 'TMSomething',
+  subject: 'Teach Me Something ',
+  width: 460,
+  height: 300,
+  theme: {
+    shell: {
+      background: 'Orange',
+      color: '#ffffff'
+    },
+    tweets: {
+      background: '#ffffff',
+      color: '#444444',
+      links: '#1985b5'
+    }
+  },
+  features: {
+    scrollbar: false,
+    loop: false,
+    live: true,
+    behavior: 'default'
+  }
+}).render().start();
+</script>
+	 
 </div>
 <?php
 include_once ("includes/foot.php");
